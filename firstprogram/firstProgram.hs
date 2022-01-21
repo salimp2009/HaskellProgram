8b2b65ac188c4477ec19838755fe2f52f56986fd
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-01-21 20:17:10 +0300

    added a number.txt with 100 number and read into a variable using do loop and <- readfile "number.txt" ; then used print and putStrLn but print print everything as a character like it prints every tab as \t and new line as \n

--- firstprogram/firstProgram.hs
+++ firstprogram/firstProgram.hs
@@ -1,3 +1,11 @@
+-- :: means has type main has type IO; which is a type class that performes input/output
+-- () mean Unit  is type containing a single value name Unit and that value is also represented by () ;  
+-- it is a tuple with zero elemnts
+-- so overall below is called as ; main hastype IO Unit; 
+-- which means main is a function that performs IO action which produces no value
 main:: IO ()  
 
-main = putStrLn "Hello Haskell Way"
+main = do 
+    content <- readFile "number.txt"
+    -- print content
+    putStrLn content
