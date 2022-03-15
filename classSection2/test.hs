0116499e2f01a90d951b3e5314f5b796843b4481
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-16 01:03:05 +0300

    added some basic function examples

--- classSection2/test.hs
+++ classSection2/test.hs
@@ -4,5 +4,19 @@ main =  do
         msg <- getLine
         putStrLn (greet msg)
 
+greeting :: [Char]
 greeting ="Hellow"
+
+greet :: [Char] -> [Char]
 greet who = greeting <> ", " <> who
+
+add :: Num a => a -> a -> a
+add a b = a + b
+
+-- this is just sameple function but lambda here is unnecessary
+add1 :: Integer -> Integer
+add1 = \x -> x + 1
+
+add' :: Integer -> Integer -> Integer
+add' = (+)
+
