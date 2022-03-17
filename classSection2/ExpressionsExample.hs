3f5c08da963fd1821968f4c53f0954c9c6d3bdb2
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-17 23:59:12 +0300

    added missing functions for the Expression; Number, Add, Subtract Also added an example for pattern matching on Lists created a newHead function

--- classSection2/ExpressionsExample.hs
+++ classSection2/ExpressionsExample.hs
@@ -4,3 +4,21 @@ data Expression = Number Int
                 | Add Expression Expression
                 | Subtract Expression Expression
                 deriving (Eq, Ord, Show)
+
+calculate:: Expression -> Int
+calculate (Number a) = a
+calculate (Add a b ) = (calculate a ) + (calculate b)         
+calculate (Subtract a b ) = (calculate a ) - (calculate b)
+
+newHead :: [p] -> p
+newHead [] = error "Empty list!"
+newHead [a] = a
+newHead (a:as) = a
+
+
+-- Use of calculate 
+{- This is a multine comment 
+calculate (Subtract (Number 1) (Number 2))
+calculate (Subtract (Number 5) (Number 3))
+calculate (Subtract (Number 50) (Add (Number 3) (Number 5)))
+-}
