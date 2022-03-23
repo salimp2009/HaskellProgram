eb543b2df10d4e8c0f384bc070312a1c66919025
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-23 23:08:00 +0300

    started adding the grid

--- wordsGame/wordsGame/src/Lib.hs
+++ wordsGame/wordsGame/src/Lib.hs
@@ -9,3 +9,14 @@ someFunc = putStrLn someString
 someString::String
 someString = "someString"
 
+-- Started Implementing the grid
+grid = [ "__C__________R___"
+       , "__SIC_________U__"
+       , "__HASKELL______B_" 
+       , "__A__A_______S__Y"
+       , "__C__________R___"
+       , "__C__________R___"
+       , "__C__________R___"
+       , "__C__________R___"
+       ]
+
