82e94f0ab7cd718f74133c6a4ecb1fce25b47bbe
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-27 01:52:28 +0300

    added new function formatGrid used unlines function that takes an array of string puts a newline; also implemented myUnlines too added grid a list of strings also added languages a list of strings revised main used outPutGrid grid; works

--- wordsGame/wordsGame/src/Lib.hs
+++ wordsGame/wordsGame/src/Lib.hs
@@ -1,22 +1,50 @@
 module Lib
-    ( someFunc
-    , someString
+    ( grid
+    , languages
+    , formatGrid
+    , outputGrid
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn someString
+--someFunc :: IO ()
+--someFunc = putStrLn someString
+
+outputGrid::[String] -> IO()
+outputGrid grid = putStrLn $ formatGrid grid
+
 
 someString::String
 someString = "someString"
 
+formatGrid::[String]-> String
+formatGrid = unlines
+
+myUnlines :: [String] -> String
+myUnlines [] = []
+myUnlines (a:as) = a ++'\n' : myUnlines as
+
 -- Started Implementing the grid
-grid = [ "__C__________R___"
-       , "__SIC_________U__"
-       , "__HASKELL______B_" 
-       , "__A__A_______S__Y"
-       , "__C__________R___"
-       , "__C__________R___"
-       , "__C__________R___"
-       , "__C__________R___"
+grid = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
        ]
 
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
