cd7a87957eb266c22ebc70117a5c3015854f3c97
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-04-22 01:20:26 +0300

    added some helper functions in TestCode to print using infix (.) function composition operator

--- wordsGame/wordsGame/src/TestCode.hs
+++ wordsGame/wordsGame/src/TestCode.hs
@@ -3,6 +3,7 @@ module TestCode where
 -- this is only for trying ideas before implementing them
 
 -- sample coordinate grid
+coords :: [[(Integer, Integer)]]
 coords = [ [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)]
          , [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)]
          , [(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7)]
@@ -14,33 +15,40 @@ coords = [ [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)]
          ]
 
 
+og:: Show a => [a] -> IO()
+og = putStrLn . unlines  . map show 
 
+-- div2 x = x `mod` 2 == 0
+div2 :: p -> Bool
+div2 x = even 2
+
+-- this is similar to coords above
+-- [(x,y) | x  <- [0..7}, y <- [0..7)]
 
 -- Copy of Data
-
-    grid :: [String]
-    grid = [ "__C________R___"
-           , "__SI________U__"
-           , "__HASKELL____B_"
-           , "__A__A_____S__Y"
-           , "__R___B___C____"
-           , "__PHP____H_____"
-           , "____S_LREP_____"
-           , "____I__M_Y__L__"
-           , "____L_E__T_O___"
-           , "_________HB____"
-           , "_________O_____"
-           , "________CN_____"
-           ]
+grid2 :: [String]
+grid2 = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
+       ]
     
-    languages = [ "BASIC"
-                , "COBOL"
-                , "CSHARP"
-                , "HASKELL"
-                , "LISP"
-                , "PERL"
-                , "PHP"
-                , "PYTHON"
-                , "RUBY"
-                , "SCHEME"
-                ]
+languages2 = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
