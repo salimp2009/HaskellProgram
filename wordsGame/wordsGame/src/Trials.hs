589fdd22cd8abd7b071bb9baeb4039d1adcd2374
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-04-28 15:26:23 +0300

    tested an example of a minimal functor List class similar to Maybe

--- wordsGame/wordsGame/src/Trials.hs
+++ wordsGame/wordsGame/src/Trials.hs
@@ -1,7 +1,4 @@
-module Trials 
-    (    mymapMaybe
-       , myUnlines
-    ) where
+module Trials where
 
 -- implementation of mapMaybe 
 mymapMaybe :: (a -> Maybe b) -> [a] -> [b]
@@ -16,4 +13,20 @@ mymapMaybe f (x:xs) =
 myUnlines :: [String] -> String
 myUnlines [] = []
 myUnlines (a:as) = a ++'\n' : myUnlines as
+
+data List a = Empty |  List a (List a) deriving (Show)
+
+
+toList:: [a] -> List a
+toList [] = Empty
+toList (a:as) = List a (toList as)
+
+fromList :: List a -> [a]
+fromList Empty = []
+fromList (List a as) = a : fromList as
+
+instance Functor List where
+  fmap _ Empty = Empty
+  fmap f (List a as) = List (f a) (fmap f as)
+ 
 
